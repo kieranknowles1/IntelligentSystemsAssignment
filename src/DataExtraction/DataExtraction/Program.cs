90c5c7283fd8d36d4d23750775388bd304dc108f
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-04-17 18:48:58 +0100

    Extract recorded audio files

--- src/DataExtraction/DataExtraction/Program.cs
+++ src/DataExtraction/DataExtraction/Program.cs
@@ -1,13 +1,37 @@
 ﻿using CsvHelper;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Archives;
 using Mutagen.Bethesda.Oblivion;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Assets;
 using Mutagen.Bethesda.Plugins.Order;
 using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace DataExtraction;
 
+using IOblivionLoadOrder = ILoadOrder<IModListing<IOblivionModGetter>>;
+
+public struct InfoIndex
+{
+    public FormKey Id { get; }
+    public int Index { get; }
+
+    public InfoIndex(FormKey id, int index)
+    {
+        Id = id;
+        Index = index;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}_{Index}";
+    }
+}
+
 public class Line
 {
-    public Line(IDialogResponseGetter response)
+    public Line(IDialogResponseGetter response, string extractedPath)
     {
         if (response.ResponseText == null || response.Data == null)
             throw new ArgumentException("Null text or data", nameof(response));
@@ -15,17 +39,38 @@ public class Line
         Text = response.ResponseText;
         EmotionType = response.Data.Emotion;
         EmotionValue = response.Data.EmotionValue;
+        ExtractedPath = extractedPath;
     }
 
     public string Text { get; }
     public EmotionType EmotionType { get; }
     public int EmotionValue { get; }
-    //public string ExtractedPath { get; }
+    public string ExtractedPath { get; }
 }
 
 public static class Program
 {
-    public static ILoadOrder<IModListing<IOblivionModGetter>> CreateLoadOrder(string dataDir, List<string> plugins)
+    public static readonly Regex VoiceRegex = new(@"sound\\voice\\(?<plugin>\w+\.es[mp])\\[a-z ]+\\[mf]\\\w+[0-9a-f]{2}(?<info>[0-9a-f]{6})_(?<index>\d+)\.mp3", RegexOptions.IgnoreCase);
+
+    /// <summary>
+    /// Get the <see cref="InfoIndex"/> of a voice files INFO record
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public static InfoIndex GetVoiceFormKey(string path)
+    {
+        var match = VoiceRegex.Match(path);
+        if (!match.Success)
+            throw new ArgumentException("Path does not match expected regex");
+
+        var formKey = FormKey.Factory($"{match.Groups["info"]}:{match.Groups["plugin"]}");
+        var index = int.Parse(match.Groups["index"].Value);
+
+        return new InfoIndex(formKey, index);
+
+    }
+
+    public static IOblivionLoadOrder CreateLoadOrder(string dataDir, List<string> plugins)
     {
         var pluginMods = plugins
             .Select(plugin => OblivionMod.CreateFromBinaryOverlay(Path.Combine(dataDir, plugin)))
@@ -34,7 +79,48 @@ public static class Program
         return new LoadOrder<IModListing<IOblivionModGetter>>(pluginMods);
     }
 
-    public static List<Line> ExtractData(List<string> plugins, string dataRoot, List<string> dataDirs)
+    public static List<Line> LinesFromLoadOrder(IOblivionLoadOrder loadOrder, List<IArchiveReader> archives, string languageCode)
+    {
+        var lines = new List<Line>();
+
+        var linkCache = loadOrder.ToImmutableLinkCache();
+
+        var byPath = archives
+            .SelectMany(bsa => bsa.Files)
+            // Use the last copy if a file exists in multiple BSAs. This is how the game handles it
+            .Reverse()
+            .DistinctBy(file => file.Path, StringComparer.OrdinalIgnoreCase)
+            .Where(file => VoiceRegex.IsMatch(file.Path))
+            .GroupBy(file => GetVoiceFormKey(file.Path))
+            .ToDictionary(group => group.Key, group => group.ToList());
+
+
+        foreach (var response in loadOrder.PriorityOrder.DialogItem().WinningOverrides())
+        {
+            for (int i = 0; i < response.Responses.Count; i++)
+            {
+                var line = response.Responses[i];
+
+                if (!byPath.TryGetValue(new(response.FormKey, i + 1), out var files))
+                    continue;
+
+                foreach (var file in files)
+                {
+                    var extractedPath = Path.Combine("voice", languageCode, file.Path);
+
+                    Console.WriteLine($"Extract file '{extractedPath}' for INFO {response.FormKey}_{i+1}");
+                    Directory.CreateDirectory(Path.GetDirectoryName(extractedPath));
+                    File.WriteAllBytes(extractedPath, file.GetBytes());
+
+                    lines.Add(new Line(line, extractedPath));
+                }
+            }
+        }
+
+        return lines;
+    }
+
+    public static List<Line> ExtractData(List<string> plugins, List<string> archives, string dataRoot, List<string> dataDirs)
     {
         var lines = new List<Line>();
 
@@ -43,14 +129,11 @@ public static class Program
             var fullPath = Path.Combine(dataRoot, dataDir);
             var loadOrder = CreateLoadOrder(fullPath, plugins);
 
-            foreach (var response in loadOrder.PriorityOrder.DialogItem().WinningOverrides())
-            {
-                foreach (var line in response.Responses)
-                {
-                    // TODO: Extract file
-                    lines.Add(new(line));
-                }
-            }
+            var thisArchives = archives
+                .Select(bsa => Archive.CreateReader(GameRelease.Oblivion, Path.Combine(fullPath, bsa)))
+                .ToList();
+
+            lines.AddRange(LinesFromLoadOrder(loadOrder, thisArchives, dataDir));
         }
 
         return lines;
@@ -68,6 +151,16 @@ public static class Program
             "Knights.esp",
         };
 
+        // Only need archives containing dialogue
+        var archives = new List<string>()
+        {
+            "Oblivion - Voices1.bsa",
+            "Oblivion - Voices2.bsa",
+            "DLCShiveringIsles - Voices.bsa",
+            "Knights.bsa",
+        };
+
+        // TODO: Dehardcode
         var dataRoot = @"E:\XboxGames\The Elder Scrolls IV- Oblivion (PC)\Content";
 
         var dataDirs = new List<string>()
@@ -79,12 +172,10 @@ public static class Program
             @"Oblivion GOTY Spanish\Data",
         };
 
-        var data = ExtractData(plugins, dataRoot, dataDirs);
+        var data = ExtractData(plugins, archives, dataRoot, dataDirs);
 
-        using (var writer = new StreamWriter("extracted_data.csv"))
-        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-        {
-            csv.WriteRecords(data);
-        }
+        using var writer = new StreamWriter("extracted_data.csv");
+        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+        csv.WriteRecords(data);
     }
 }
