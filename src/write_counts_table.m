70ffa1988819728b7f046a06ed3603f9d139f14c
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2023-04-27 20:27:10 +0100

    Fix scripts

--- src/write_counts_table.m
+++ src/write_counts_table.m
@@ -4,12 +4,12 @@ function write_counts_table(annotations, file)
     names = categories(annotations);
     counts = countcats(annotations);
     total = sum(counts);
-    fractions = counts / total;
+    fractions = round(counts / total, 2);
     for i = 1:length(names)
         fprintf('%s: %d\n', names{i}, counts(i));
     end
 
-    countsTable = table(names, counts, fractions, 'VariableNames', ["Name", "Count", "Fraction"]);
-    countsTable(end+1, :) = {"Total", total, 1.0};
+    countsTable = table(names, counts, fractions, 'VariableNames', ["Name", "Count", "Proportion"]);
+    countsTable(end+1, :) = {'Total', total, 1.0};
     writetable(countsTable, file);
 end
